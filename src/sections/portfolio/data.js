eccf180cf5a50325d4741ee65ac025015bf118a2
Sampy <abdoulsa95@gmail.com>
2024-02-07 12:55:24 +0000

    portfolio update

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,17 +1,25 @@
-import Image1 from '../../assets/project009.png'
-
+import Image1 from "../../assets/project009.png";
+import Image2 from "../../assets/image2.png";
 
 const data = [
-    {
-        id: 1,
-        category: 'FullStack',
-        image: Image1,
-        title: "InvoiceFlow (FullStack)",
-        desc: "InvoiceFlow is a web based application built with angular.JS for the front-end, springboot for the back-end and a Mysql database. It is a system that keep tracks of customers and bill them.",
-        demo: '#',
-        github: 'mailto:abdoulsa95@gmail.com'
-    }
-]
+  {
+    id: 1,
+    category: "FullStack",
+    image: Image1,
+    title: "InvoiceFlow (FullStack)",
+    desc: "InvoiceFlow is a web based application built with angular.JS for the front-end, springboot for the back-end and a Mysql database. It is a system that keep tracks of customers and bill them.",
+    demo: "#",
+    github: "mailto:abdoulsa95@gmail.com",
+  },
+  {
+    id: 2,
+    category: "FullStack",
+    image: Image2,
+    title: "FocusIT (FullStack)",
+    desc: "FocusIT is a web based application built with react.JS for the front-end, node.JS and Express.JS for the back-end and a Mongodb database. It is a system that keep tracks of job interviews.",
+    demo: "#",
+    github: "mailto:abdoulsa95@gmail.com",
+  },
+];
 
-
-export default data
+export default data;
